d76a5519d53f77e41d53d5c4f56b97555573ae25
Arin Soni <arinsoni2809@gmail.com>
2021-06-28 23:37:00 +0530

    Arin

---
+++ arin/android/app/src/main/kotlin/com/example/arin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- arin2/android/app/src/main/kotlin/com/example/arin2/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.arin2
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
